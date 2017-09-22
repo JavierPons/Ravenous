93ca5342eb828dae59440cc1c03760bc3ea4209f
Javier Pons <jpescandell@hotmail.com>
2017-09-22 20:57:55 +0300

    second commit working

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,14 +1,13 @@
 const clientId = 'T6fLamDIKEIJ_vqsK3DgAg';
 const secret = 'lAdoOEdrtIL2BoKgKquZiE8btvvBLqSPzE8v41clKmoAsR2qW0w1F6p5mDtpD1bN';
-let accessToken = "";
+let accessToken = '';
 
 
 const Yelp = {
   getAccessToken() {
     if (accessToken) {
-      return new Promise(resolve => {
-        resolve(accessToken);
-      });
+      return new Promise(resolve => 
+        resolve(accessToken));
     }
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
        method: 'POST'
@@ -25,7 +24,7 @@ const Yelp = {
        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
         headers: { Authorization: `Bearer ${accessToken}` 
       }
-      });
+     
       
       }).then(response => {
       return response.json();
@@ -40,14 +39,15 @@ const Yelp = {
               city: business.location.city,
               state: business.location.state,
               zipCode: business.location.zipcode,
-              category: business.categories.title,
+              category: business.categories[0].title,
               rating: business.rating,
               reviewCount: business.review_count
             }));
       }
     });
+    });
   }
-};
+}
 
 
 export default Yelp;
